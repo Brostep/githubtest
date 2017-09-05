166d180653988659c506a02de579ccd3cc041ecd
Gaston <7ZGaston@users.noreply.github.com>
2017-09-05 18:32:26 -0300

    position=this.transform

--- GameTest/Assets/Scripts/PlayerMovement.cs
+++ GameTest/Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,12 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody rb;
 	Transform position;
-<<<<<<< HEAD
-	public Transform position2;
 
-=======
->>>>>>> c88c97a34c91dc3f010ad3d19284670320f01ad1
 	// rb
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 
-		position = position2;
+		position = this.Transform;
 
 		//positiondelposition...
 	}
