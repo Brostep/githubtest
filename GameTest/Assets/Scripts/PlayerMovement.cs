7c875d30fbbb363fd78e6b45e7cca60e4fd97efd
Brostep <ltr.pereyra@gmail.com>
2017-09-05 18:32:43 -0300

    position = position2;

--- GameTest/Assets/Scripts/PlayerMovement.cs
+++ GameTest/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody rb;
 	Transform position;
+	Transform position2;
 	// rb
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		position = position2;
 	}
 }
