f76f2db0dd8090627daf58c8bfd793688c9b0d3c
Gaston <7ZGaston@users.noreply.github.com>
2017-09-05 18:21:43 -0300

    position=position2

--- GameTest/Assets/Scripts/PlayerMovement.cs
+++ GameTest/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody rb;
+	Transform position;
+	public Transform position2;
 
 	// rb
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+
+		position = position2;
+
+		//positiondelposition...
 	}
 }
